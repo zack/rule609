ddae1c06bc73c95a6ec617925c4a9f0c62758721
Zack Youngren <github.com@youngren.io>
2025-09-16 01:13:20 -0400

    remove react-compiler from plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 'babel-plugin-react-compiler'],
+  plugins: [react()],
 })
